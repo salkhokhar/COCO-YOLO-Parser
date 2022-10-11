d6f1f53b84e04e437f76493b922d9d9bd3c06f2d
salkhokhar <61519106+salkhokhar@users.noreply.github.com>
2022-10-12 02:57:27 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -48,7 +48,15 @@ def get_image_and_annotation(args):
     coco = COCO(args.anno)
 
     # list of category that you want to train
-    target = ['car', 'motorcycle']
+    target = [ 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+         'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+         'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+         'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+         'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+         'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+         'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+         'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+         'hair drier', 'toothbrush' ]
 
     create_obj_name_file(target)
     # need a mapping table to find the correspondence between coco catId and custom catId
